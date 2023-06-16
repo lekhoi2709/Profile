9510359fa94ccd12aef8a29018a6557a06d39fa8
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-06-16 23:34:39 +0700

    change title

--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -19,7 +19,7 @@ export default function Navigator() {
     <nav className="flex items-center justify-around w-full h-16 fixed z-20 backdrop-blur-[7px] bg-[#e7e7e7]/50 dark:bg-[#111111]/70">
       <div className="flex gap-4 items-center">
         <Link href="/" scroll={false}>Leko</Link>
-        <Link href="/work" scroll={false}>Work</Link>
+        <Link href="/projects" scroll={false}>Projects</Link>
         <Link href="/source" scroll={false}>Source</Link>
       </div>
       <Toggle onClick={changeTheme} className="object-cover p-2 dark:text-[#404565] dark:bg-[#dcdaca] rounded-md bg-[#404565] text-[#dcdaca]">
