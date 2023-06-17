7477b03d1a73e15e500b6ced474ad1b4ad2345e1
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-06-17 10:25:11 +0700

    add github link

--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link"
-import { useEffect, useState } from "react"
 import { Toggle } from "@radix-ui/react-toggle"
-import { Sun, Moon } from "lucide-react"
+import { Sun, Moon, Github } from "lucide-react"
 import { useTheme } from "next-themes"
 
 export default function Navigator() {
@@ -13,10 +12,10 @@ export default function Navigator() {
 
   return (
     <nav className="flex items-center justify-around w-full h-16 fixed z-20 backdrop-blur-[7px] bg-[#e7e7e7]/50 dark:bg-[#111111]/70">
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-6 items-center">
         <Link href="/">Leko</Link>
         <Link href="/projects">Projects</Link>
-        <Link href="/source">Source</Link>
+        <Link className="flex gap-1 items-center" href="https://github.com/lekhoi2709/Profile"><Github width={15} height={15} />Source</Link>
       </div>
       <Toggle onClick={changeTheme} className="object-cover p-2 dark:text-[#404565] dark:bg-[#dcdaca] rounded-md bg-[#404565] text-[#dcdaca]">
         {theme == "dark" ? <Sun /> : <Moon />}
